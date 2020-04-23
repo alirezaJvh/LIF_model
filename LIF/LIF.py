0d9c0bc00ecb5b4a3da4d9f5b0f7e57c5f4b0c34
alireza <alireza.jvh98@gmail.com>
2020-04-24 01:11:07 +0430

    make private some method

--- LIF/LIF.py
+++ LIF/LIF.py
@@ -10,21 +10,23 @@ class LIF:
         self.end_time = parameters['end_time']
         self.dt = parameters['dt']                    # step size (ms)
 
-
-    def tau_m(self):
+    def __tau_m(self):
         return self.C * self.R
 
     def time_length(self):
         return self.end_time - self.start_time
 
-    def interval(self):
-        return self.tau_m() * (np.log((self.R * self.I) / (self.R * self.I - self.threshold)))
+    def interval(self, I):
+        array = np.empty(len(I))
+        for i in range(len(I)):
+            array[i] = self.__tau_m() * (np.log((self.R * I[i]) / (self.R * I[i] - self.threshold)))
+        return array
 
-    def make_time_array_steps(self):
+    def __make_time_array_steps(self):
         return np.arange(0, self.end_time + self.dt, self.dt)
 
     def simulate(self):
-        time = self.make_time_array_steps()
+        time = self.__make_time_array_steps()
         u_history = np.empty(len(time))
         self.I_history = np.empty(len(time))
         self.I_history[0] = 0
@@ -36,24 +38,25 @@ class LIF:
                 self.I_history[i] = 0
             else:
                 # tau_m * (du/dt) = -(u - u_rest) + (R * I)
-                du = self.tau_m() * ((-u_history[i-1] + self.u_rest) + (self.R * self.I))
+                du = self.__tau_m() * ((-u_history[i-1] + self.u_rest) + (self.R * self.I))
                 u_history[i] = u_history[i-1] + du * self.dt
                 self.I_history[i] = self.I
-                print(u_history[i])
+                # print(u_history[i])
 
             if u_history[i] > self.threshold:
-                self.reset_model(u_history, i)
+                self.__reset_model(u_history, i)
                 spike = spike + 1
 
         print(spike)
         return {"u": u_history, "time": time}
 
-    def reset_model(self, u, index):
+    def __reset_model(self, u, index):
         u[index] = self.u_rest
         u[index - 1] = self.threshold
 
     def get_I_history(self):
-        return self.I_history;
+        return self.I_history
+
 
 
 

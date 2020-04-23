a6439539ae36c034983bda0f83996a958934c6a1
alireza <alireza.jvh98@gmail.com>
2020-04-24 00:11:07 +0430

    add I_history

--- LIF/LIF.py
+++ LIF/LIF.py
@@ -26,32 +26,34 @@ class LIF:
     def simulate(self):
         time = self.make_time_array_steps()
         u_history = np.empty(len(time))
-        spike = 0
+        self.I_history = np.empty(len(time))
+        self.I_history[0] = 0
         u_history[0] = self.u_rest
+        spike = 0
         for i in range(1, len(time)):
             if time[i] < self.start_time:
                 u_history[i] = self.u_rest
-                print(1)
+                self.I_history[i] = 0
             else:
                 # tau_m * (du/dt) = -(u - u_rest) + (R * I)
                 du = self.tau_m() * ((-u_history[i-1] + self.u_rest) + (self.R * self.I))
-                # print(du)
-                # print('****')
                 u_history[i] = u_history[i-1] + du * self.dt
+                self.I_history[i] = self.I
                 print(u_history[i])
 
             if u_history[i] > self.threshold:
-                u_history[i] = self.u_rest
-                u_history[i-1] = self.threshold
+                self.reset_model(u_history, i)
                 spike = spike + 1
 
         print(spike)
         return {"u": u_history, "time": time}
 
-    def time_I_info(self):
-        pass
+    def reset_model(self, u, index):
+        u[index] = self.u_rest
+        u[index - 1] = self.threshold
 
-
+    def get_I_history(self):
+        return self.I_history;
 
 
 
